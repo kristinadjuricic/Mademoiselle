9e8f06a245566ff50f2b8660b4b528c2c2d32845
Kristina Djuricic <kristinadjuricic11@gmail.com>
2020-03-27 19:09:31 +0100

    Team section is finished

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -10,7 +10,37 @@ $(document).ready(function () {
         checkHeader();
     });
 
-
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animation = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < scroll + windowHeight - 100) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
+            }
+        });
+    }
+    animation();
+    $(window).scroll(function () {
+        animation();
+        animateHeader();
+    });
+    
+    if($('.team-slider').length > 0){
+        $('.team-slider').owlCarousel({
+            autoplay: true,
+            loop:true,
+            
+            responsive:{
+                0:{
+                    items: 1
+                }
+            }
+        });
+    }
 
 });
 
