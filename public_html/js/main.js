8205a7de59e759724c97194ee03f3746c533d7bc
Kristina Djuricic <kristinadjuricic11@gmail.com>
2020-04-01 16:58:19 +0200

    Corrections

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -36,6 +36,58 @@ $(document).ready(function () {
             loop:true
         });
     }
+    
+        if ($('.contact-form').length > 0) {
+
+        $(function () {
+            $(".contact-form").validate({
+                highlight: function (element) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
+                },
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                     subject: {
+                        required: true
+                    },
+                    yourMessage: {
+                        required: true
+                    }
+
+                },
+                messages: {
+
+                    name: {
+                        required: 'Please provide your name.'
+                    },
+                    email: {
+                        required: 'The Email is required field.',
+                        email: 'Please provide a valid Email address.'
+                    },
+                    subject: {
+                        required: 'The Subject is required field.'
+                    },
+                    yourMessage: {
+                        required: 'Your message is required field.'
+                    }
+
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                    error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
+                }
+
+            });
+        });
+    }
 
 });
 
